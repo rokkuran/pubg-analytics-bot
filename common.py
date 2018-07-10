77d1467f14c9c0aa03bc126978b4cc92779e8e67
Unknown <lachlanbtaylor@gmail.com>
2018-07-10 13:14:39 +1000

    matplotlib heroku issue: added function

--- common.py
+++ common.py
@@ -47,18 +47,18 @@ class Query(API):
 
 		return match_info
 	
-	# def test_plot(self, n):
+	def test_plot(self, n):
 	
-	# 	matplotlib.pyplot.plot(range(n), np.random.normal(0, 1, n))
-	# 	fig = matplotlib.pyplot.gcf()
+		matplotlib.pyplot.plot(range(n), np.random.normal(0, 1, n))
+		fig = matplotlib.pyplot.gcf()
 
-	# 	buf = io.BytesIO()
-	# 	fig.savefig(buf, format='png')
-	# 	buf.seek(0)
-	# 	string = base64.b64encode(buf.read())
+		buf = io.BytesIO()
+		fig.savefig(buf, format='png')
+		buf.seek(0)
+		string = base64.b64encode(buf.read())
 
-	# 	uri = 'data:image/png;base64,' + urllib.parse.quote(string)
-	# 	return uri
+		uri = 'data:image/png;base64,' + urllib.parse.quote(string)
+		return uri
 
 	def get_user_last_match(self, username):
 		players = self.api.players().filter(player_names=[username])
