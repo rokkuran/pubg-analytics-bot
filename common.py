f5c5496de85f5886e14cd038148218d6033c4ec0
Unknown <lachlanbtaylor@gmail.com>
2018-07-13 14:44:35 +1000

    return first event

--- common.py
+++ common.py
@@ -69,47 +69,47 @@ class Query(API):
 		telemetry = self._get_telemetry(match_id)
 		return telemetry.events_from_type(event_type)
 
-
 	def get_player_kill_events(self, match_id):
 		events = self._get_events(match_id, 'LogPlayerKill')
-		return str(events)
+		return events
 
 	def get_player_attack_events(self, match_id):
 		# telemetry = self._get_telemetry(match_id)
 		# events = telemetry.events_from_type('LogPlayerAttack')
 		events = self._get_events(match_id, 'LogPlayerAttack')
 		# return events
+		return events[0].attacker_name, events[0].attack_type, events[0].weapon.name
 		
-		attack_points_header = ["timestamp", "attack_id", "attacker_name", "attack_type", "weapon_vehicle"]
-		attack_points = []
+		# attack_points_header = ["timestamp", "attack_id", "attacker_name", "attack_type", "weapon_vehicle"]
+		# attack_points = []
 
-		for attack in events:
+		# for attack in events:
 			
-			if attack.weapon.name != "Undefined":
-				attack_point = [
-					self._convert_timestamp(attack.timestamp),
-					attack.attack_id,
-					attack.attacker.name,
-					attack.attack_type,
-					attack.weapon.name
-				]
+		# 	if attack.weapon.name != "Undefined":
+		# 		attack_point = [
+		# 			self._convert_timestamp(attack.timestamp),
+		# 			attack.attack_id,
+		# 			attack.attacker.name,
+		# 			attack.attack_type,
+		# 			attack.weapon.name
+		# 		]
 
-				attack_points.append(attack_point)
+		# 		attack_points.append(attack_point)
 				
-			if attack.vehicle.name != "Undefined":
+		# 	if attack.vehicle.name != "Undefined":
 
-				attack_point = [
-					self._convert_timestamp(attack.timestamp),
-					attack.attack_id,
-					attack.attacker.name,
-					attack.attack_type,
-					attack.vehicle.name
-				]
+		# 		attack_point = [
+		# 			self._convert_timestamp(attack.timestamp),
+		# 			attack.attack_id,
+		# 			attack.attacker.name,
+		# 			attack.attack_type,
+		# 			attack.vehicle.name
+		# 		]
 				
-				attack_points.append(attack_point)
+		# 		attack_points.append(attack_point)
 				
 
-		return pd.DataFrame(attack_points, columns=attack_points_header)
+		# return pd.DataFrame(attack_points, columns=attack_points_header)
 
 	
 
